fec11865edf12ec2b1c3622af98f182199254501
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-08-30 16:16:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,48 @@
-import telebot
 import requests
-from telebot.types import InlineKeyboardButton as Btn , InlineKeyboardMarkup as Mak
-	
-token = "5853600134:AAEBZF6ZgSrpjYT2XnJKRwyLtt-EOtRV43Q"#token
+import telebot
+from user_agent import generate_user_agent
+token = "5853600134:AAEBZF6ZgSrpjYT2XnJKRwyLtt-EOtRV43Q"
 bot = telebot.TeleBot(token)
 
-@bot.message_handler(func=lambda brok:True)
-def Url(message):
-		try:
-			msgg = bot.send_message(message.chat.id, "*جاري التحميل ...*",parse_mode="markdown")
-			msg = message.text
-			url = requests.get(f'https://tikwm.com/api/?url={msg}').json()
-			music = url['data']['music']
-			region = url['data']['region']
-			tit = url['data']['title']
-			vid = url['data']['play']
-			ava = url['data']['author']['avatar']
-			##
-			name = url['data']['music_info']['author']
-			time = url['data']['duration']
-			sh = url['data']['share_count']
-			com = url['data']['comment_count']
-			wat = url['data']['play_count']
-			bot.delete_message(chat_id=message.chat.id, message_id=msgg.message_id)
-			bot.send_photo(message.chat.id,ava,caption=f'- اسم الحساب : *{name}*\n - دوله الحساب : *{region}*\n\n- عدد مرات المشاهدة : *{wat}*\n- عدد التعليقات : *{com}*\n- عدد مرات المشاركة : *{sh}*\n- طول الفيديو : *{time}*',parse_mode="markdown")
-			btn = Mak().add(Btn('تحميل الصوت',url=''+music))
-			bot.send_video(message.chat.id,vid, caption=f"{tit}",reply_markup=btn)
-		except:
-			pass
-			bot.delete_message(chat_id=message.chat.id, message_id=msgg.message_id)
-			bot.reply_to(message,'error );')
+headers = {
+'authority': 'reelsaver.net',
+'accept': '*/*',
+'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+'origin': 'https://reelsaver.net',
+'referer': 'https://reelsaver.net/download-reel-instagram',
+'sec-ch-ua': '"Chromium";v="105", "Not)A;Brand";v="8"',
+'sec-ch-ua-mobile': '?1',
+'sec-ch-ua-platform': '"Android"',
+'sec-fetch-dest': 'empty',
+'sec-fetch-mode': 'cors',
+'sec-fetch-site': 'same-origin',
+'user-agent': generate_user_agent(),
+'x-requested-with': 'XMLHttpRequest',
+}#@Crrazy_8 & @BRoK8
+@bot.message_handler(commands=["start"])
+def Welcome(message):
+ name = message.from_user.first_name
+ bot.reply_to(message,f'''- مرحبا بك [{name}](tg://settings)
+- في بوت تحميل من الانستكرام 
+- لتحميل (الفديوهات) ارسل رابط المنشور''',parse_mode="markdown")
+@bot.message_handler(content_types=['text'])
+def video(message):
+    link = message.text
+    data = {
+    'via': 'form',
+    'ref': 'download-reel-instagram',
+    'url': link,
+    }#@Crrazy_8 & @BRoK8
+    response = requests.post('https://reelsaver.net/api/instagram', headers=headers, data=data).json()
+    No = response['success']
+    if No == False:
+     bot.reply_to(message,'<i>خطأ تأكد من الرابط ..</i>',parse_mode="html")
+    else:
+     bot.send_chat_action(message.chat.id,action='upload_video')
+     #user = response['data']['user']['username']
+     video = response["data"]['medias'][0]['src']
+     #log = response['data']['medias'][0]['preview_src']
+     bot.send_video(message.chat.id,video , reply_to_message_id=message.message_id)
 
-
-print('run')
 bot.infinity_polling()
-# تذكرو مصدري 
-'''
-مبرمج الملف - @BRoK8
-قناتي - @Crrazy_8
-'''
