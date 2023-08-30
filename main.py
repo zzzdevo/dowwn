412900acb91ce28176ea9edafd0c2115fe859d0e
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-08-30 13:22:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import requests, os
-from pyrogram import *
-from pyrogram.types import *
-try:
-	import yt_dlp
-except:
-	os.system("pip install yt_dlp")
-	import yt_dlp
+import requests
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import yt_dlp
 	
 api_id = 12962251 # Here Api Id 
 api_hash = "b51499523800add51e4530c6f552dbc8" # Here Api Hash 
@@ -18,7 +14,7 @@ def start(client, message):
  do = requests.get(f"https://api.telegram.org/bot{bot_token}/getChatMember?chat_id=@MGIMT&user_id={message.from_user.id}").text
     
  if do.count("left") or do.count("Bad Request: user not found"):
-  return message.reply_text("**ببورە ئەزیزم [کەناڵی بۆت](t.me/MGIMT) پێویستە سەرەتا جۆینی کەناڵ بکەیت بۆ بەکارهێنانی بۆت\nدواتر /start بکە♥️✅**",
+  return message.reply_text("**ببورە ئەزیزم پێویستە سەرەتا جۆینی [کەناڵی بۆت](t.me/MGIMT) بکەیت بۆ بەکارهێنانی بۆت\nدواتر /start بکە♥️✅**",
   disable_web_page_preview=True,
   reply_markup=InlineKeyboardMarkup(
   [[InlineKeyboardButton("⚡️ جۆینی کەناڵ بکە ئەزیزم ⚡️",
@@ -49,7 +45,6 @@ def start(client, message):
 @app.on_message(filters.text & filters.private)
 async def download(client, message):
      EnyWeb = message.text 
-     Me = message.from_user.mention
      x = await message.reply("**دادەبەزێت کەمێك چاوەڕێبە . . . ❗️**")
      try:
        Media = yt_dlp.YoutubeDL({'format': 'best[ext=mp4]'}).extract_info(EnyWeb, False)["url"]
